a3327246ecdbe3e3464846e6531cfc111286e609
Elizabeth <dentdelion@yandex.ru>
2016-07-22 18:20:53 +0300

    how-i-work section for mobile

--- resources/views/partials/landing/how-i-work.blade.php
+++ resources/views/partials/landing/how-i-work.blade.php
@@ -1,5 +1,5 @@
 <section class="howiworks wrap" id="howiwork">
-    <div class="container">
+    <div class="container hidden-xs">
         <div class="row">
             <h2 class="title">HOW I WORK</h2>
         </div>
@@ -123,6 +123,220 @@
                 </div>
             </div>
         </div>
+    </div>
+    <div class="container visible-xs">
+        <div class="row">
+            <h2 class="title">HOW I WORK</h2>
+        </div>
+        <div class="button-container">
+        <div class="row">
+            <div class="col-xs-6">
+                <a data-toggle="modal" data-target="#chat">
+                            <div class="blocks-how-mobile">
+                                <img src="img/chat.png" class="image-how">
+                                <p>Chatting<br /> with client</p>
+                                
+                            </div>
+                </a>
+            </div>
+            <div class="col-xs-6">
+                <a data-toggle="modal" data-target="#task">
+                            <div class="blocks-how-mobile">
+                                <img src="img/task.png" class="image-how">
+                                <p>Technical<br /> task</p>
+                                
+                            </div>
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-6">
+                <a data-toggle="modal" data-target="#analytics">
+                            <div class="blocks-how-mobile">
+                                <img src="img/analytics.png" class="image-how">
+                                <p>Analytics</p>
+                                
+                            </div>
+                </a>
+            </div>
+            <div class="col-xs-6">
+                <a data-toggle="modal" data-target="#prototyping">
+                            <div class="blocks-how-mobile">
+                                <img src="img/prototype.png" class="image-how">
+                                <p>Prototyping</p>
+                                
+                            </div>
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-6">
+                <a data-toggle="modal" data-target="#code">
+                            <div class="blocks-how-mobile">
+                                <img src="img/code.png" class="image-how">
+                                <p>Coding &amp; <br> fixing bugs</p>
+                                
+                            </div>
+                </a>
+            </div>
+            <div class="col-xs-6">
+                <a data-toggle="modal" data-target="#product">
+                            <div class="blocks-how-mobile">
+                                <img src="img/product.png" class="image-how">
+                                <p>Ready<br />product</p>
+                                
+                            </div>
+                </a>
+            </div>
+        </div>
+        </div>
+    </div>
+
+        <!-- Modal -->
+    <div id="chat" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">CHATTING WITH CLIENT</h4>
+              </div>
+              <div class="modal-body">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="border-modal-btn" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+
+          </div>
+    </div>
+    <div id="task" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">TECHNICAL TASKS</h4>
+              </div>
+              <div class="modal-body">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="border-modal-btn" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+
+          </div>
+    </div>
+    <div id="analytics" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">ANALYTICS</h4>
+              </div>
+              <div class="modal-body">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="border-modal-btn" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+
+          </div>
+    </div>
+    <div id="prototyping" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">PROTOTYPING</h4>
+              </div>
+              <div class="modal-body">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="border-modal-btn" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+
+          </div>
+    </div>
+    <div id="code" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">CODING &amp; FIXING BUGS</h4>
+              </div>
+              <div class="modal-body">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="border-modal-btn" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+
+          </div>
+    </div>
+    <div id="product" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">READY PRODUCT</h4>
+              </div>
+              <div class="modal-body">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="border-modal-btn" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+
+          </div>
+    </div>
     </div>
 </section>
        
