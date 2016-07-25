0a3d3baf937aa03679e891634514199b6e8d77e4
Elizabeth <dentdelion@yandex.ru>
2016-07-25 14:16:17 +0300

    rewritten footer styles

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,12 +2,23 @@
 <footer class="wrap">
     <div class="container">
         <div class="row">
-            <div class="col-xs-12 col-md-4 col-md-offset-2">
-                <p class="ft-email">EMAIL: <span>talk@kievdev.com</span></p>
-            </div>
-            <div class="col-xs-12 col-md-5 ">
-                <p class="ft-phone">PHONE: <span class="phone">+38 (091) 964 58 55</span></p>
-            </div>
+            
+                
+                <div class="col-md-offset-1 col-md-2 col-xs-12">
+                <p class="ft-email">EMAIL:</p>
+                </div>
+                <div class="col-md-2 email">
+                <span>talk@kievdev.com</span>
+                </div>
+            
+            
+                <div class="col-md-offset-1 col-md-2 col-xs-12">
+                <p class="ft-phone">PHONE:</p>
+                </div>
+                <div class="col-md-3 phone">
+                <span>+38 (091) 964 58 55</span>
+                </div>
+                
         </div>
     </div>
 </footer>
