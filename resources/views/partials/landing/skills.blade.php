4b121de9bf19cbbb954c673da05833a89d3ff392
Elizabeth <dentdelion@yandex.ru>
2016-07-29 16:06:21 +0300

    fixed bug with skills section on the mobile screen

--- resources/views/partials/landing/skills.blade.php
+++ resources/views/partials/landing/skills.blade.php
@@ -9,7 +9,7 @@
         <div class="row charts">
             <div class="col-sm-offset-1 col-sm-2">
                 <div class=row>
-                <div class="col-xs-offset-7 col-xs-1 percents">
+                <div class="col-xs-1 percents">
                 25%
                 </div>
                 </div>
@@ -33,7 +33,7 @@
             </div>
             <div class="col-sm-2">
                 <div class=row>
-                    <div class="col-xs-offset-7 col-xs-1 percents">
+                    <div class="col-xs-1 percents">
                         75%
                     </div>
                 </div>
@@ -55,7 +55,7 @@
             </div>
             <div class="col-sm-2">
                 <div class=row>
-                    <div class="col-xs-offset-7 col-xs-1 percents">
+                    <div class="col-xs-1 percents">
                     90%
                     </div>
                 </div>
@@ -77,7 +77,7 @@
             </div>
             <div class="col-sm-2">
                 <div class=row>
-                    <div class="col-xs-offset-7 col-xs-1 percents">
+                    <div class="col-xs-1 percents">
                     100%
                     </div>
                 </div>
@@ -99,7 +99,7 @@
             </div>
             <div class="col-sm-2">
                 <div class=row>
-                    <div class="col-xs-offset-7 col-xs-1 percents">
+                    <div class="col-xs-1 percents">
                     10%
                     </div>
                 </div>
@@ -121,40 +121,7 @@
             </div>
         </div>
 
-        <!--<div class="row skill-wrapper">
-            <div class="col-sm-3 col-sm-offset-3 col-xs-5 col-xs-offset-1 lang-name">
-                <p>HTML<span>/CSS</span></p>
-            </div>
-            <div class="col-sm-3 col-xs-5 progress-line"></div>
-            <div class="col-sm-1 col-xs-12">
-                <div class="percentage">
-                    <span>100%</span>
-                </div>
-            </div>
-        </div>
-        <div class="row skill-wrapper">
-            <div class="col-sm-3 col-sm-offset-3 col-xs-5 col-xs-offset-1 lang-name">
-                <p>PHP</p>
-            </div>
-            <div class="col-sm-3 col-xs-5 progress-line"></div>
-            <div class="col-sm-1 col-xs-12">
-                <div class="percentage">
-                    <span>100%</span>
-                </div>
-            </div>
-        </div>
-        <div class="row skill-wrapper">
-            <div class="col-sm-3 col-sm-offset-3 col-xs-5 col-xs-offset-1 lang-name">
-                <p>JAVA</p>
-            </div>
-            <div class="col-sm-2 col-xs-3 progress-line"></div>
-            <div class="col-sm-1 col-xs-12">
-                <div class="percentage">
-                    <span>65%</span>
-                </div>
-            </div>
-        </div>
-        -->
+       
     </div>
 </section>
 </div>
