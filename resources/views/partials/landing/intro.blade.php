8f1ea898bcce6d2702b16d2bca7157eafad0613a
Elizabeth <dentdelion@yandex.ru>
2016-07-29 16:11:51 +0300

    Fixed bug with intro section on the mobile version. Background image instead of video

--- resources/views/partials/landing/intro.blade.php
+++ resources/views/partials/landing/intro.blade.php
@@ -1,8 +1,24 @@
-    <!-- todo: use divs below
-        <div class="first-bg"></div>
-        <div class="main-screen-bg"></div>
-    -->
-<div id="intro">
+    
+<div id="intro" class="visible-xs">
+    <div class="first-bg"></div>
+    <div class="main-screen-bg"></div>
+    <section class="intro wrap">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <h1>SVYATOSLAV SVITLYCHNYI</h1>
+                    <h3>WEB DEVELOPMENT / CONSULTING</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-ms-12 col-xs-12">
+                    <button class="btn contact-me" id='hire-me'>HIRE ME</button>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+<div id="intro" class="hidden-xs">
   <div id="video-wrap">
     <video id="cover-video" preload="metadata" autoplay loop>
         <source src="cover/landing/Hello-World.mp4" type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.
@@ -22,20 +38,6 @@
        </div>
     -->
 
-    <section class="intro wrap">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12">
-                    <h1>SVYATOSLAV SVITLYCHNYI</h1>
-                    <h3>WEB DEVELOPMENT / CONSULTING</h3>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-ms-12 col-xs-12">
-                    <button class="btn contact-me" id='hire-me'>HIRE ME</button>
-                </div>
-            </div>
-        </div>
-    </section>
+    
   </div>
 </div>
