fd45a0aed09af9ee6a6bed6d06e730679a259a0c
kolya <hrynko.kolya@gmail.com>
2016-10-07 15:10:20 +0300

    validate blog/1 v2

--- resources/views/partials/post/post.blade.php
+++ resources/views/partials/post/post.blade.php
@@ -12,7 +12,7 @@
                     <div class="row">
                         <div class="col-sm-12">
                             <h1>{{$post->title}}</h1>
-                            <h4>{{$post->created_at}}</h4>
+                            {{--<h4>{{$post->created_at}}</h4>--}}
                             <p>
                             {!! $post->content !!}
                             </p><hr/>
